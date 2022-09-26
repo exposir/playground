9be61fa0978807de25a9552af6a5db507bbf4cc0
孟世博 <897205285@qq.com>
2022-09-26 15:18:34 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const git = require("simple-git");
 
 const init = async () => {
-  try {
-    const simpleGit = git();
-    await simpleGit.add("./*");
-    await simpleGit.commit("update");
-    await simpleGit.push("origin", "main");
-  } catch (e) {
-    console.log(e);
-  }
+  const submit = async () => {
+    try {
+      const simpleGit = git();
+      await simpleGit.add("./*");
+      await simpleGit.commit("update");
+      await simpleGit.push("origin", "main");
+      console.log("ok");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  setTimeout(() => {
+    submit();
+  }, 5000);
 };
 
 init();
