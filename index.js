99f2a02ea9ad54cf9b83baffe7a733c978edbfd0
孟世博 <897205285@qq.com>
2022-09-26 15:15:34 +0800

    update

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const git = require("simple-git");
 
 const init = async () => {
   try {
-    // const ROOT = "/Users/menglingyu/My/playground";
     const simpleGit = git();
-
     await simpleGit.add("./*");
     await simpleGit.commit("update");
     await simpleGit.push("origin", "main");
