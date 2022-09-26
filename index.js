60f997ad251ba9aaa5dde6d4e2b705dd4d7a6c1f
孟世博 <897205285@qq.com>
2022-09-26 15:13:24 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const simpleGit = require("simple-git");
+const git = require("simple-git");
 
 const init = async () => {
   try {
+    const ROOT = "/Users/menglingyu/My/playground";
+    const simpleGit = git(ROOT);
+    // await simpleGit.status();
+    // console.log(status);
+
+    // await simpleGit.pull("orgin", main);
+    await simpleGit.add("./*");
+    await simpleGit.commit("update");
+
     // const a = await git.init();
     // console.log(a);
-
-    simpleGit()
-      .add("./*")
-      .commit("first commit!")
-      .addRemote("origin", "some-repo-url")
-      .push();
-
+    // simpleGit()
+    //   .add("./*")
+    //   .commit("first commit!")
+    //   .addRemote("origin", "some-repo-url")
+    //   .push();
     // await git.addRemote(name, repoUrl);
   } catch (e) {
     /* handle all errors here */
